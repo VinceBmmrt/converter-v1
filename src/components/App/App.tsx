590e802925ecca1deaaf8952c095c55714b550ea
Aspitrine <loquente62@gmail.com>
2023-10-25 12:17:07 +0200

    Passage des données likes et setLikes dans le composant Footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import ResultConversion from '../ResultConversion/ResultConversion';
 import './App.scss';
 
 import currenciesData from '../../data/currencies';
+import Footer from '../Footer/Footer';
 
 function App() {
   // Pour créer une variable Reactive avec React, il faut utiliser le hook (fonction) `useState`.
@@ -14,14 +15,6 @@ function App() {
   // La convention veux que la fonction de modification se nomme `set` + le nom de la variable
   const [likesCount, setLikesCount] = useState(0);
 
-  // Fonction qui sera exécuter au click sur le bouton Like
-  const handleClickLikeBtn = () => {
-    // Je modifie la valeur de likesCount en lui rajouter +1
-    // Cela va déclencher une mise à jour du composant
-    // La fonction App va de nouveau être exécuter
-    setLikesCount(likesCount + 1);
-  };
-
   return (
     <div className="app">
       <BaseAmount />
@@ -29,13 +22,7 @@ function App() {
       <Currencies currencies={currenciesData} />
       <ResultConversion />
 
-      <footer>
-        {/* Pour ajouter des eventListener, il suffit d'ajouter un `onQQCH` sur l'élément en question */}
-        {/* Dans notre exemple onClick, je lui passe la fonction a exécuter */}
-        <button type="button" onClick={handleClickLikeBtn}>
-          Like ❤️ : {likesCount}
-        </button>
-      </footer>
+      <Footer likesCount={likesCount} setLikesCount={setLikesCount} />
     </div>
   );
 }
