3f80bb45f6dccfd07128ee866a0f6f9a135fc1d5
Aspitrine <loquente62@gmail.com>
2023-10-26 13:48:16 +0200

    Synchronisation entre ma donnée currency et l'API pour récupérer le taux d'échange

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -21,12 +21,38 @@ function App() {
   // On peut définir le type de notre variable d'état sans passé de valeur initial
   // Il faut préciser le type entre `<>` après le useState et avatn les ()
   const [currency, setCurrency] = useState<Currency>(currenciesData[0]);
+  const [rate, setRate] = useState(0);
 
   // Je modifie le titre de la page lorsque la devise change
   // useEffect permet d'exécuter une action (fonction) lorsqu'une variable change
   useEffect(() => {
     console.log('La devise a changé, je change mon title');
     document.title = `${currency.description} - React Currency Converter`;
+
+    fetch(
+      `http://api.exchangerate.host/convert?from=EUR&to=${currency.code}&amount=1&access_key=f2fd6ae06abf0999da92bf08591832c8`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // data correspond à donnée retourner par l'API qui ressemble à
+        // {
+        //   "success": true,
+        //   "terms": "https://currencylayer.com/terms",
+        //   "privacy": "https://currencylayer.com/privacy",
+        //   "query": {
+        //     "from": "EUR",
+        //     "to": "USD",
+        //     "amount": 1
+        //   },
+        //   "info": {
+        //     "timestamp": 1698315123,
+        //     "quote": 1.05493
+        //   },
+        //   "result": 1.05493
+        // }
+        // Depuis les données récupérer de mon API, je stock la valeur de la conversion dans une variable d'état
+        setRate(data.result);
+      });
     // Lorsque mon composant est exécuter la première fois ET lorsque la variable currency change
   }, [currency]);
 
@@ -42,10 +68,7 @@ function App() {
       {/* Je fournis la liste des currencies récupérer depuis mon fichier data/currencies.js */}
       {/* Je passe la fonction setCurrency qui sera exécuter ... au click sur la devise */}
       <Currencies currencies={currenciesData} onClickCurrency={setCurrency} />
-      <ResultConversion
-        value={currency.rate}
-        currencyName={currency.description}
-      />
+      <ResultConversion value={rate} currencyName={currency.description} />
 
       <Footer likesCount={likesCount} setLikesCount={setLikesCount} />
     </div>
